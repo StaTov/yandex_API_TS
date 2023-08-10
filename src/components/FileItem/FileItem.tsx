30e190fc1b93d401e40d6c1a1c188c835a1ca420
StaTov <tovchfamily@gmail.com>
2023-08-10 20:03:54 +0400

    add style

--- src/components/FileItem/FileItem.tsx
+++ src/components/FileItem/FileItem.tsx
@@ -8,12 +8,11 @@ interface Props {
 const FileItem = ({ data }: Props) => {
 
     const iconUrl = ('preview' in data) ? data.preview : ''
-
     return (
         <div className='file_item'>
-            <div><img src={iconUrl} referrerPolicy='no-referrer' alt=""></img></div>
-            <div>{data.name}</div>
-            <div>{data.size}</div>
+            <div><img src={iconUrl} alt={data.name} width='100px' /></div>
+            <div className='file_item__name'>{data.name}</div>
+            <div className='file_item__size'>{data.size}</div>
         </div>
     )
 }
